e70156f3966039e9b9c58b67cacf19ecbeb67684
Ridvan Ozaydin <ridvan.ozaydin@siemens.com>
2017-11-09 17:23:42 +0300

    STSer initial commit

---
+++ src/com/rhtech/stser/STSer.java
@@ -0,0 +1,15 @@
+package com.rhtech.stser;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class STSer extends AnAction
+{
+
+    @Override
+    public void actionPerformed(AnActionEvent e)
+    {
+        // TODO: insert action logic here
+        System.out.println("My First idea Plugin");
+    }
+}
