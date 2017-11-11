a4beb034ca0f03149fb661f5d606d4dea6d656ee
rozaydin <ridvanozaydin@gmail.com>
2017-11-11 12:18:17 +0300

    updated plugin with reflection

--- src/com/rhtech/stser/STSer.java
+++ src/com/rhtech/stser/STSer.java
@@ -1,7 +1,12 @@
 package com.rhtech.stser;
 
+import com.intellij.execution.RunConfigurationExtension;
 import com.intellij.execution.RunManager;
 import com.intellij.execution.application.ApplicationConfiguration;
+import com.intellij.execution.configurations.ModuleBasedConfiguration;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.execution.configurations.RunConfigurationBase;
+import com.intellij.ide.ui.EditorOptionsTopHitProvider;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
@@ -14,6 +19,7 @@ import com.intellij.openapi.project.ProjectManager;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.lang.reflect.Method;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -21,21 +27,18 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-public class STSer extends AnAction
-{
+public class STSer extends AnAction {
     final String STS_CONFIG_NAME = "sts.txt";
     final String STS_CONFIG_DIR = "STS";
 
-    enum TOKEN
-    {
+    enum TOKEN {
         AWS_ACCESS_KEY_ID,
         AWS_SECRET_ACCESS_KEY,
         AWS_SECURITY_TOKEN,
         AWS_SESSION_TOKEN
     }
 
-    private Optional<Map<TOKEN, String>> parseFile()
-    {
+    private Optional<Map<TOKEN, String>> parseFile() {
 
         String homeDirectory = System.getProperty("user.home");
         Path path = Paths.get(homeDirectory, STS_CONFIG_DIR, STS_CONFIG_NAME);
@@ -43,37 +46,30 @@ public class STSer extends AnAction
         File configFile = path.toFile();
         Map<TOKEN, String> AWS_ENV_VARS = new HashMap<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(configFile)))
-        {
+        try (BufferedReader reader = new BufferedReader(new FileReader(configFile))) {
 
             int tokenSearchIndex = 0;
             String line;
-            while ((line = reader.readLine()) != null)
-            {
+            while ((line = reader.readLine()) != null) {
 
                 TOKEN currToken = TOKEN.values()[tokenSearchIndex];
                 String searchExpression = currToken + "=";
                 int searchExpIndex = line.indexOf(searchExpression);
 
-                if (searchExpIndex != -1)
-                {
+                if (searchExpIndex != -1) {
                     AWS_ENV_VARS.put(currToken, line.substring(searchExpIndex + searchExpression.length()));
-                    if ((++tokenSearchIndex) >= TOKEN.values().length)
-                    {
+                    if ((++tokenSearchIndex) >= TOKEN.values().length) {
                         break;
                     }
                 }
             }
 
             // if collected token count is less than expected
-            if (AWS_ENV_VARS.size() < TOKEN.values().length)
-            {
+            if (AWS_ENV_VARS.size() < TOKEN.values().length) {
                 throw new Exception("AWS Environment variables are missing (expected 4 but found less)");
             }
 
-        }
-        catch (Exception exc)
-        {
+        } catch (Exception exc) {
             ApplicationManager.getApplication().executeOnPooledThread(() -> {
                 Notifications.Bus.notify(new Notification("STSer", "STS Parsing Error ", "Unable to Parse STS token file exc: " + exc.getMessage(),
                         NotificationType.ERROR));
@@ -84,9 +80,19 @@ public class STSer extends AnAction
         return AWS_ENV_VARS.size() == TOKEN.values().length ? Optional.of(AWS_ENV_VARS) : Optional.empty();
     }
 
+    private void modifyEnvVarSettings(Object target, Map<TOKEN, String> awsTokens) throws Exception {
+
+        // if configuration type does not have getEnvs method this call will fail
+        Method method = target.getClass().getMethod("getEnvs");
+        Map<String, String> envVars = (Map<String, String>) method.invoke(target);
+
+        awsTokens.entrySet().stream().forEach((entry) -> {
+            envVars.put(entry.getKey().name(), entry.getValue());
+        });
+    }
+
     @Override
-    public void actionPerformed(AnActionEvent e)
-    {
+    public void actionPerformed(AnActionEvent e) {
 
         parseFile().ifPresent((awsTokens) -> {
 
@@ -94,18 +100,24 @@ public class STSer extends AnAction
 
             Arrays.stream(openProjects).forEach((project) -> {
                 RunManager runManager = RunManager.getInstance(ProjectManager.getInstance().getOpenProjects()[0]);
-                if (runManager.getSelectedConfiguration() != null)
-                {
-                    ApplicationConfiguration config = (ApplicationConfiguration) runManager.getSelectedConfiguration().getConfiguration();
-                    awsTokens.entrySet().stream().forEach((entry) -> {
-                        config.getEnvs().put(entry.getKey().name(), entry.getValue());
-                    });
-                    // display notification
-                    ApplicationManager.getApplication().executeOnPooledThread(() -> {
-                        Notifications.Bus.notify(new Notification("STSer", "Run Configuration " + config.getName() + " updated",
-                                "STS variables set to env variables", NotificationType.INFORMATION));
-                    });
+                if (runManager.getSelectedConfiguration() != null) {
+                    RunConfiguration runConfig = runManager.getSelectedConfiguration().getConfiguration();
+                    try {
+                        modifyEnvVarSettings(runConfig, awsTokens);
 
+                        // display notification
+                        ApplicationManager.getApplication().executeOnPooledThread(() -> {
+                            Notifications.Bus.notify(new Notification("STSer", "Run Configuration " + runConfig.getName() + " updated",
+                                    "STS variables set to env variables", NotificationType.INFORMATION));
+                        });
+
+                    } catch (Exception exc) {
+                        // display notification
+                        ApplicationManager.getApplication().executeOnPooledThread(() -> {
+                            Notifications.Bus.notify(new Notification("STSer", "Run Configuration type:" + runConfig.getType().getDisplayName() + " is not supported",
+                                    "Configuration Type is not supported", NotificationType.INFORMATION));
+                        });
+                    }
                 }
             });
         });
